2cd64f8fc84402ae892837fa94b8e2849825c800
[Jade]KimJaeSeong <jade16work@gmail.com>
2025-05-15 13:03:14 +0900

    [Feat] PooledObject

--- Assets/Scripts/DesignPatterns/PoolTest.cs
+++ Assets/Scripts/DesignPatterns/PoolTest.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 
 public class PoolTest : MonoBehaviour
 {
-    // [SerializeField] private AudioManager _audioManagerPrefab;
-    // private ObjectPool<> _pool
-    // private PooledObject _temp;
+    [SerializeField] private PooledObjectTest _prefab;
+    private ObjectPool _pool;
+    private PooledObject _temp;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool(_prefab, 10);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _temp = _pool.PopPool();
+            _temp.transform.parent = transform;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _temp.ReturnPool();
+            // 혹은 _pool.PushPool(_temp);
+        }
+    } 
 
 
 }
